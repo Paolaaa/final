1dc40d487d3988a33d59e2b9a24bdcbe747406b4
Paola Garcia <garciaamezola@gmail.com>
2015-03-21 00:00:32 +0000

    I was only able to do 3 of the exercises. I had a really hard time on all of them so just getting those 3 took a very long time

--- final.py
+++ final.py
@@ -89,3 +89,51 @@ it worksfec00007_3.seq
 it worksfec00007_2.seq
 
 #so os.listdir only gives me a list of the names of the files in the directory but it doesn't read the files so i can format the names but no the actual file content.
+vagrant@data-science-toolbox:~$ cd final
+vagrant@data-science-toolbox:~/final$ mkdir files_inn
+vagrant@data-science-toolbox:~/final$ mkdir files_nex
+vagrant@data-science-toolbox:~/final$ mkdir files_parm
+vagrant@data-science-toolbox:~/final$ mkdir files_treefile
+#the previous created the new directories for my files
+
+vagrant@data-science-toolbox:~/final$ mv ~/final/shell_fun/*.inn ~/final/files_inn
+vagrant@data-science-toolbox:~/final$ mv ~/final/shell_fun/*.nex ~/final/files_nex
+vagrant@data-science-toolbox:~/final$ mv ~/final/shell_fun/*.parm ~/final/files_parm
+vagrant@data-science-toolbox:~/final$ mv ~/final/shell_fun/*.tree ~/final/files_treefiles
+#separated the files in shell_fun according to their type of file and moved them to separate directories
+
+vagrant@data-science-toolbox:~/final$ grep -r ".nex"* ~/final/files_nex/ >> nex_files
+#find all the files that have .nex in the files_nex directory and then output the results to a new file called nex_files
+
+# I am not sure what it is you are asking me to do in number 4
+
+curl 'http://dailybruin.com/category/news/' >dailybruin.html
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(open("dailybruin.html"))
+news_pane = soup.find(class = "medium-8 columns section-left") #i made the mistake of not using an underscore after class
+
+news_pane = soup.find(class_ = "medium-8 columns section-left")
+titles = news_pane.find_all("div", class_="row db-list")
+for names in titles:
+    names = div.find("article", id="post-")
+    news_title = div.find("h2")
+    print news_title #i get error message that div is not defined
+for names in titles:
+    names = div.find("article", id="post-")
+    news_title = article.find("h2")
+    print news_title #same error as before
+for div in titles:
+    names = div.find("article", id="post-")
+    news_title = article.find("h2")
+    print news_title #error message article is not defined
+for div in titles:
+    names = div.find("article", id="post-215786")
+    news_title = article.find("h2")
+    print news_title #same error message that article is not defined
+
+
+
+
+
+
+
